16b797ca91f2c995d11abb8b02fe3c62cf054854
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 15:27:58 +0000

    Integrated friendly routes for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -23,17 +23,14 @@ class BlogsController < ApplicationController
 
   # POST /blogs
   # POST /blogs.json
-
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
-      if @blog.update(blog_params)
+        if @blog.update(blog_params) 
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -51,7 +48,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
---
+++ db/migrate/20170622143038_add_slugs_to_blogs.rb
@@ -0,0 +1,4 @@
+class AddSlugsToBlogs < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
