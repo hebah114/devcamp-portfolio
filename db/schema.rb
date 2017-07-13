4e164ef27025b4babcaeea54b967b2eb863a70a0
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 17:44:06 +0000

    Added instalation requirments for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170711161653) do
-=======
-
-ActiveRecord::Schema.define(version: 20170705172631) do
->>>>>>> e30ea0edeb52d6da7fb66e58ebfae9faa25a4f55
+ActiveRecord::Schema.define(version: 20170713173956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +70,7 @@ ActiveRecord::Schema.define(version: 20170705172631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -90,29 +85,11 @@ ActiveRecord::Schema.define(version: 20170705172631) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "roles"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-<<<<<<< HEAD
   add_foreign_key "blogs", "topics"
   add_foreign_key "technologies", "portfolios"
-=======
-  create_table "views", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_views_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true, using: :btree
-  end
->>>>>>> e30ea0edeb52d6da7fb66e58ebfae9faa25a4f55
 end
