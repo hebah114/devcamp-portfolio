476c4957d557417e66431594372ed64ca308fcf1
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 16:09:19 +0000

    Implemented partial for admin actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,5 +1,5 @@
 module BlogsHelper
-    class CodeRayify < Redcarpet::Render::HTML
+  class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
       CodeRay.scan(code, language).div
     end
@@ -17,5 +17,9 @@ module BlogsHelper
 
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
+  end
+
+  def blog_status_color blog
+    'color: red;' if blog.draft?
   end
 end
